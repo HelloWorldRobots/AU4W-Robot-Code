cf841bb290cc59264acc23ed068072e91ddd945c
HelloWorldRobo <67485645+HelloWorldRobo@users.noreply.github.com>
2020-06-28 17:59:53 -0400

    Update 4WD_ROBOT.ino

--- 4WD_ROBOT.ino
+++ 4WD_ROBOT.ino
@@ -1,6 +1,7 @@
 /* Created at 6/19/2020 | Experiments with Robot (4WD) | Developed by: Akshat Sharma | */
-/* Bluetooth Date: 2-Forward, 3-Backward, 4-Right, 5-Left, 6-Auto, 1-LED*/
 
+
+/* Defining the pins and other variables */
 int info;
 const int LED_A = 12;
 const int LED_B = 13;
@@ -21,8 +22,9 @@ int near = 8;
 int Follow = 20;
 int Point = 35;
 
-
+/* This is the setup for the robot, which also contains the "center-point" for the code */
 void setup() {
+  /* Setting up all the pins */
   pinMode(LED_A, OUTPUT);
   pinMode(LED_B, OUTPUT);
   pinMode(LOWER_LED, OUTPUT);
@@ -40,32 +42,43 @@ void setup() {
   digitalWrite(LED_A, LOW);
   digitalWrite(LED_B, LOW);
   
+  /* Booting Up LED (Used to determine errors) */
   for(int p = 0; p < 8; p++){
   digitalWrite(LOWER_LED, HIGH);
   delay(350);
   digitalWrite(LOWER_LED, LOW);
   delay(350);
   }
+  /* Serial Begins at 9600 Baud Rate */
   Serial.begin(9600);
+  /* The main "loop" that runs the program together */
   while(true){
-  
+  /* Functions called in this loop, can be later seen below in the code */
   BlinkLED();
+  /* Checks, if there is signal from Bluetooth (connected to the Serial Pins) */
   if(Serial.available() > 0){
+    /* Reads the Signal */
     info = Serial.read();
    }
+    /* Checks, the signal and executes the action respectively */
     if(info == '2'){
+        /* Defining "long" inches and using it to detect any nearby object's, while in Manual Control of the Robot*/
         long inches;
         Object_Detection(inches);
+        /* Obstacle deteced, procceds to safety */
         if(inches < near){
            backward();
+           /* Goes backward for 300 microsecs */
            delay(300);
            stops();
          }
+        /* Light's UP, when recieved the command */
         Light_UP();
         Serial.println("Forward");
         forward();
+        /* Goes forward for 680 MicroSecs */
         delay(680);
-       
+        /* Checks Again for Obstacles */
         Object_Detection(inches);
         if(inches < near){
            backward();
@@ -73,13 +86,14 @@ void setup() {
            stops();
          }
         stops();
+        /* And checks again for more accuracry */
         Object_Detection(inches);
         if(inches < near){
            backward();
            delay(300);
            stops();
          }
-      
+        /* Reset's the signal (info) */
         info = 0;
     }
         else if(info == '3'){
@@ -149,25 +163,30 @@ void setup() {
               info = 0;
             }
         else if(info == '6'){
+          /* Check Box on App, to initate Night Mode */  
            NightMode();
-           info == 0;
+           info = 0;
         }
         else if(info == '1'){
+          /* Emergency Stop */
           Light_UP();
           stops();
           info = 0;
           
         }
         else if(info == '7'){
+          /* Autonomus Mode - Goes to a dedicated point */
            Light_UP();
            Go_To_Point_A();
         }
          else if(info == '8'){
+          /* Autonomus Mode - Obstacle Avoider */
           Light_UP();
           AutoMode();
           
         }
          else if(info == '9'){
+          /* Autonomus Mode - Follow's an Object */
           Light_UP();
           
           FollowObjectAuto();
@@ -180,21 +199,27 @@ void setup() {
 }
   
 void loop() {
-
+/* Empty Loop */
 
 }
+/* Start of the funcs */
 
+// Forward //
 void forward(){
+  /* Changed The Volt's to determine the movement of the Motors */ 
+  // HIGH - 5V or 3.3V for 3.3V and LOW for 0V //
+  // Front Motors //
   digitalWrite(ForwardR, HIGH);
   digitalWrite(ForwardL, LOW);
   digitalWrite(BackwardR, HIGH);
   digitalWrite(BackwardL, LOW);
+  // Back Motors //
   digitalWrite(a, HIGH);
   digitalWrite(c, LOW);
   digitalWrite(b, HIGH);
   digitalWrite(d, LOW);
 }
-
+// Backward //
 void backward(){
   digitalWrite(ForwardR, LOW);
   digitalWrite(ForwardL, HIGH);
@@ -205,7 +230,7 @@ void backward(){
   digitalWrite(b, LOW);
   digitalWrite(d, HIGH);
 }
-
+// Left //
 void left(){
   digitalWrite(ForwardR, HIGH);
   digitalWrite(ForwardL, LOW);
@@ -217,6 +242,7 @@ void left(){
   digitalWrite(d, LOW);
   
 }
+// Right //
 void right(){
   digitalWrite(ForwardR, LOW);
   digitalWrite(ForwardL, HIGH);
@@ -228,6 +254,7 @@ void right(){
   digitalWrite(d, HIGH);
 }
 
+// Stop //
 void stops(){
    digitalWrite(ForwardR, LOW);
   digitalWrite(ForwardL, LOW);
@@ -240,25 +267,29 @@ void stops(){
   
 }
 
+// Obstacle Avoidance Function //
 void AutoMode(){
+  /* Check's the inches */
   long inches;
   Object_Detection(inches);
   
 
  
    if(inches > range){
-    
+    /* Goes Forward till there is no obstacle */
     forward();
    
     Object_Detection(inches);
     
     }
+    /* Found Obstacle, Avoids it */
     if(inches < range){  
       Serial.println(inches);
       backward();
       delay(1555);
       stops();
-      tone(13, 699, 650);
+      /* Tone func, to make sound (piezo) when object averted */
+      tone(10, 699, 650);
       delay(150);
       right();
       delay(1000);
@@ -271,24 +302,29 @@ void AutoMode(){
    }
 
  
-
+/* Follow The Object */
 void FollowObjectAuto(){
+  /* Check's the range of the object */
    long inches;
    Object_Detection(inches);
   if(inches > Follow){
+    /* If inches is less then Follow, turn right and left to begin searching for the object */
      for(int m; m < 3; m++){
+       /* Turn's right slowly stopping in different angles, to find object */
         right();
         delay(250);
         stops();
         Object_Detection(inches);
        
           if(inches < Follow){
+            /* Found Object ? Break the for loop !*/
                 break;
           }
         delay(250);
     }
   
       for(int g; g < 6; g++){
+       /* Turn's left slowly stopping in different angles, to find object */
         left();
         delay(250);
         stops();
@@ -296,6 +332,7 @@ void FollowObjectAuto(){
         Object_Detection(inches);
      
         if(inches < Follow){
+           /* Found Object ? Break the for loop !*/
            break;
         }
         delay(250);
@@ -303,10 +340,11 @@ void FollowObjectAuto(){
    
       }
  }
+  /* After, breaking the for, loop check again and go forward till the object is less then the range (Follow) */
   if(inches < Follow){
     
     forward();
-
+    /* Checks again, for obstacle */
     Object_Detection(inches);
     
     
@@ -314,29 +352,25 @@ void FollowObjectAuto(){
   
 
 }
+/* Go's to designated point */
 void Go_To_Point_A(){
   
-  Go_Straight_Until_Obstacle();
-  
-  
-}
 
-void Go_Straight_Until_Obstacle(){
+  /* Check's if there is obstacle */
    long inches;
    Object_Detection(inches);
-  
-  
+  /* Repeat this for 3 times to get to the designated point */
+  for(int v; v < 3; v++){
+
   if(inches > Point){
+    /* If there is no obstacle, keep going forward */
     forward();
     long inches;
     Object_Detection(inches);
     
-  
-    
-    
-  
-  }
+    }
   if(inches < Point){
+    /* If, there is obstacle turn right */
     right();
     delay(680);
     stops();
@@ -344,9 +378,15 @@ void Go_Straight_Until_Obstacle(){
     Object_Detection(inches);
     
     
+    }
   }
+  
+  
 }
 
+
+
+/* Simple Code to Blink LED, Uses Voltage to turn "on" and "off" */
 void BlinkLED(){
   digitalWrite(LED_A, HIGH);
   digitalWrite(LED_B, HIGH);
@@ -356,36 +396,45 @@ void BlinkLED(){
   delay(350);
 }
 
+/* Used to Light the Front LEDS */
 void Light_UP(){
   digitalWrite(LED_A, HIGH);
   digitalWrite(LED_B, HIGH);
 }
 
+/* Buzzer, when going backward */
 void Buzzer(){
   for(int r = 0; r < 3; r++){
    tone(10, 250, 250);
     
   }
 }
+
 void Obstacle_Buzz(){
    tone(10, 699);
 }
+/* TURN's on the Lower LED */
 void NightMode(){
   digitalWrite(LOWER_LED, HIGH);
 }
+/* From MicroSecs to Inches - To calculate distance from object (ultrasonic) */ 
 long microsecondsToInches(long microseconds){
  return microseconds / 74 / 2;
 }
 
-void Object_Detection(long &inches){
-     long duration;
+/* Object Detection */
+
+void Object_Detection(long &inches //Pointer to use Inches outside the function// ){
+    /* Long Duration */
+    long duration;
+    /* This is used to generate a clear signal */
     digitalWrite(TrigPin, LOW);
     delayMicroseconds(2);
     digitalWrite(TrigPin, HIGH);
     delayMicroseconds(5);
     digitalWrite(TrigPin, LOW);
     
+    /* Check's the pulses (HIGH, LOW) */
     duration = pulseIn(EchoPin, HIGH);
-      
     inches = microsecondsToInches(duration);
 }
